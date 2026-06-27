e31c65fde9db23a63c097a65a37c4a21e75c91c8
0jayer <ahnafojayer03@gmail.com>
2026-06-28 03:00:03 +0600

    remove old experimental files

--- google_drive.py
+++
@@ -1,6 +0,0 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from cloud_storage import CloudStorage
-
-class GoogleDrive(CloudStorage):
-    pass
--- test.py
+++
@@ -1,6 +0,0 @@
-import shutil
-
-with open ("result.txt", encoding='utf-8') as f:
-    print(f.read())
-
-shutil.move("F:/multidrive/result.txt", "F:/multidrive/test")
